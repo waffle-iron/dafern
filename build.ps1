9f594c5898b5b0328b454816a13d9b1fed4103bb
Nano Miratus <nanomiratus@gmail.com>
2017-06-30 19:07:31 +0200

    build | meta | html output verändert, konvertiert cover jetzt nicht mehr in dataURI

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 param(
-    [switch]$nopdf = $false,
-    [switch]$nohtml = $false,
-    [switch][alias("d")]$debug = $false
+	[switch]$nopdf = $false,
+	[switch]$nohtml = $false,
+	[switch][alias("d")]$debug = $false
 )
 
 
@@ -10,115 +10,122 @@ $files = Get-ChildItem "src" -Filter chapter*.md
 
 $output = ""
 
+if (!$debug) {
+	$params = '--quiet '
+}
+else {
+	$params = ''
+}
+
 function quots2Chevrons($text) {
-    $text -replace '"(.*?)"', '»$1«'
+	$text -replace '"(.*?)"', '»$1«'
 }
 
 function buildMd ($type) {
 
-    if ($type -eq "first") {
-        $params = '-s '
+	if ($type -eq "first") {
+		$params += '-s '
 
-        $listOfFiles = ".\src\metadata.md "
-    }
-    else {
-        $listOfFiles = ".\build\metadata.md "
-        $params = ''
+		$listOfFiles = ".\src\metadata.md "
+	}
+	else {
+		$listOfFiles = ".\build\metadata.md "
+		$params += ''
 
-        $title = "# Dafern"
+		$title = "# Dafern"
 
-        $metadata = Get-Content .\src\metadata.md
+		$metadata = Get-Content .\src\metadata.md
 
-        $newMetadata1 = $title
+		$newMetadata1 = $title
 
-        $newMetadata1 += $("" | Out-String)
+		$newMetadata1 += $("" | Out-String)
 
-        $newMetadata1 += $("" | Out-String)
+		$newMetadata1 += $("" | Out-String)
 
-        $newMetadata2 = ""
+		$newMetadata2 = ""
 
-        $newMetadata2 += $metadata
+		$newMetadata2 += $metadata
 
-        $newMetadata2 = $newMetadata2 -replace '\-\-\-(.*?)\-\-\-', ''
+		$newMetadata2 = $newMetadata2 -replace '\-\-\-(.*?)\-\-\-', ''
 
-        $newMetadata2 = $newMetadata2 -replace '\{(.*?)\}', ''
+		$newMetadata2 = $newMetadata2 -replace '\{(.*?)\}', ''
 
-        $newMetadata2 = $newMetadata2 -replace '!\[\]\(\.\\images\\daferncover\.png\)', '![](/images/daferncover.png)'
+		$newMetadata2 = $newMetadata2 -replace '!\[\]\(\.\\images\\daferncover\.png\)', '![](/images/daferncover.png)'
 
-        $newMetadata = $newMetadata1
+		$newMetadata = $newMetadata1
 
-        $newMetadata += $newMetadata2
+		$newMetadata += $newMetadata2
 
-        Set-Content .\build\metadata.md $newMetadata
-    }
+		Set-Content .\build\metadata.md $newMetadata
+	}
 
-    for ($i = 0; $i -lt $files.Count; $i++) {
-        $input = Get-Content $files[$i].FullName -Raw
-        $output = quots2Chevrons $input
-        $output = $output -replace "\# ([\w, ]+)*", '# $1 {-}'
-        $output += $("" | Out-String)
+	for ($i = 0; $i -lt $files.Count; $i++) {
+		$input = Get-Content $files[$i].FullName -Raw
+		$output = quots2Chevrons $input
+		$output = $output -replace "\# ([\w, ]+)*", '# $1 {-}'
+		$output += $("" | Out-String)
     
-        Set-Content .\build\$($files[$i].Name) $output
+		Set-Content .\build\$($files[$i].Name) $output
 
-        $listOfFiles += ".\build\"
-        $listOfFiles += $files[$i].Name
-        $listOfFiles += " "
-    }
+		$listOfFiles += ".\build\"
+		$listOfFiles += $files[$i].Name
+		$listOfFiles += " "
+	}
 	
-    $params += '--from=markdown+emoji --to=markdown --atx-headers --wrap=none --preserve-tabs'
+	$params += '--from=markdown+emoji --to=markdown --atx-headers --wrap=none --preserve-tabs'
 
-    $command = 'pandoc'
-    $command += ' '
-    $command += $params
-    $command += ' '
-    $command += $listOfFiles # already has space after it
-    $command += '-o build\dafern.md'
+	$command = 'pandoc'
+	$command += ' '
+	$command += $params
+	$command += ' '
+	$command += $listOfFiles # already has space after it
+	$command += '-o build\dafern.md'
 
-    Invoke-Expression $command
+	Invoke-Expression $command
 
-    if ($type -eq "first") {
-    }
-    else {
-        $document = Get-Content .\build\dafern.md -Raw
-        $document = ($document) -replace '\#(?! Dafern)', '##'
-        $document = ($document) -replace ' \{(.*?)\}', ''
-        $document = ($document) -replace '(?!^)\[\^(.*?)\]', '<sup id="a$1">[$1](#f$1)</sup>'
-        $document = ($document) -replace '<sup(.*?)\[(.*?)\](.*?)sup>: (.*?)\n', '<b id="f$2">[$2](#a$2)</b>: $4'
-        $document = ($document) -replace '<b id="f1">\[1\]\(\#a1\)</b>', "---`n<b id='f1'>[1](#a1)</b>"
+	if ($type -eq "first") {
+	}
+	else {
+		$document = Get-Content .\build\dafern.md -Raw
+		$document = ($document) -replace '\#(?! Dafern)', '##'
+		$document = ($document) -replace ' \{(.*?)\}', ''
+		$document = ($document) -replace '(?!^)\[\^(.*?)\]', '<sup id="a$1">[$1](#f$1)</sup>'
+		$document = ($document) -replace '<sup(.*?)\[(.*?)\](.*?)sup>: (.*?)\n', '<b id="f$2">[$2](#a$2)</b>: $4'
+		$document = ($document) -replace '<b id="f1">\[1\]\(\#a1\)</b>', "---`n<b id='f1'>[1](#a1)</b>"
 
-        Set-Content .\build\dafern.md $document
+		Set-Content .\build\dafern.md $document
 
-        if (!$debug) {
-            Remove-Item .\build\metadata.md
-        }
+		if (!$debug) {
+			Remove-Item .\build\metadata.md
+		}
         
-    }
-    if (!$debug) {
-        Remove-Item .\build\chapter*.md
-    }
+	}
+	if (!$debug) {
+		Remove-Item .\build\chapter*.md
+	}
 }
 
 function buildFromMd($format) {
-    $params = '.\build\dafern.md --from=markdown+emoji --atx-headers --normalize --preserve-tabs --reference-location=block'
+	$params += '.\build\dafern.md --from=markdown+emoji --atx-headers --normalize --preserve-tabs --reference-location=block'
 
-    if ($format -eq "pdf") {
-        $params += ' --template=".\templates\default.tex" -o build\dafern.pdf'
-    }
-    elseif ($format -eq "html") {
-        $params += ' --self-contained --to=html5 -o build\dafern.html'
-    }
+	if ($format -eq "pdf") {
+		$params += ' --template=".\templates\default.tex" -o build\dafern.pdf'
+	}
+	elseif ($format -eq "html") {
+		$params += ' --standalone --to=html5 -o build\dafern.html'
+	}
 
-    $command = 'pandoc '
-    $command += $params
-    Invoke-Expression $command
+	$command = 'pandoc '
+	$command += $params
+	Invoke-Expression $command
 }
 
 buildMd "first"
 if (!$nopdf) {
-    buildFromMd "pdf"
+	buildFromMd "pdf"
 }
 
 if (!$nohtml) {
-    buildFromMd "html"
+	buildFromMd "html"
 }
 buildMd
